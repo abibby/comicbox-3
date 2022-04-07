1947f3720ab9c90c6f174121eb37eb662c215a36
Adam Bibby <adam_bibby@hotmail.com>
2022-04-07 09:50:17 -0400

    upcate appleStatusBarStyle

--- ui/lib/create-html.ts
+++ ui/lib/create-html.ts
@@ -47,6 +47,7 @@ export default function createHTMLPlugin(options: Options): Plugin {
                 appShortName: manifest.short_name,
                 start_url: manifest.start_url,
                 theme_color: manifest.theme_color,
+                appleStatusBarStyle: 'black',
             }
             const faviconsPromise = generateFavicons(iconPath, {
                 ...faviconOptions,
